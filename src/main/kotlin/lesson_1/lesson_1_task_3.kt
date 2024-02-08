f2afb96899c7cddf3c5a70148cb7c7e0ef6c2ea3
Pavel <paveld95@mail.ru>
2024-02-08 17:43:31 +0300

    KS-1-3 Gagarin's flight

---
+++ src/main/kotlin/lesson_1/lesson_1_task_3.kt
@@ -0,0 +1,21 @@
+package org.example.lesson_1
+
+fun main() {
+
+    val year: UShort = 1961u
+    var hour: UByte = 9u
+    var minute: UByte = 7u
+
+    println(year)
+    println(hour)
+    println(minute)
+
+    hour = 10u
+    minute = 55u
+
+//    println("$hour:$minute")
+    print(hour)
+    print(":")
+    print(minute)
+
+}
