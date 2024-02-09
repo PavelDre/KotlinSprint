0a8f1b18a97710469eb715c5c0e37f8da12ab29e
Pavel <paveld95@mail.ru>
2024-02-09 23:14:17 +0300

    KS-1-2 fixed the error

--- src/main/kotlin/lesson_1/lesson_1_task_2.kt
+++ src/main/kotlin/lesson_1/lesson_1_task_2.kt
@@ -3,7 +3,7 @@ package org.example.lesson_1
 fun main() {
 
     val numberOfOrders: UByte = 75u
-    val feedbackUser : String = "Большое спасибо за поставленный товар!"
+    val feedbackUser: String = "Большое спасибо за поставленный товар!"
 
     println(numberOfOrders)
     println(feedbackUser)
