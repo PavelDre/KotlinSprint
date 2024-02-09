e64f2a37adaa2ffdda660e56f2839964bf66fcd2
Pavel <paveld95@mail.ru>
2024-02-10 00:55:48 +0300

    KS-2-1 average rating

---
+++ src/main/kotlin/lesson_2/lesson_2_task_1.kt
@@ -0,0 +1,9 @@
+package org.example.lesson_2
+
+fun main() {
+
+    val averageRating: Double = (3 + 4 + 3 + 5) / 4.0
+//    val averageRating: Float = (3 + 4 + 3 + 5) / 4f
+
+    println(averageRating)
+}
