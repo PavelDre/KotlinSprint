e9d96e47cc302b9bdb493c9fa004889bb7af9216
Pavel <paveld95@mail.ru>
2024-02-12 13:05:20 +0300

    KS-2-3 added constant

--- src/main/kotlin/lesson_2/lesson_2_task_3.kt
+++ src/main/kotlin/lesson_2/lesson_2_task_3.kt
@@ -5,10 +5,12 @@ fun main() {
     val trainDepartureTimeHours: Byte = 9
     val trainDepartureTimeMinutes: Byte = 39
     val trainTravelTimeMinutes: Short = 457 // 7 часов 37 мин
-    val trainArrivalTimeHours = trainDepartureTimeHours + ((trainDepartureTimeMinutes + trainTravelTimeMinutes) / 60)
-    val trainArrivalTimeMinutes = (trainDepartureTimeMinutes + trainTravelTimeMinutes) % 60
+    val trainArrivalTimeHours = trainDepartureTimeHours + ((trainDepartureTimeMinutes + trainTravelTimeMinutes) / MINUTES_IS_HOUR)
+    val trainArrivalTimeMinutes = (trainDepartureTimeMinutes + trainTravelTimeMinutes) % MINUTES_IS_HOUR
 
     print(trainArrivalTimeHours)
     print(":")
     print(trainArrivalTimeMinutes)
 }
+
+const val MINUTES_IS_HOUR = 60
