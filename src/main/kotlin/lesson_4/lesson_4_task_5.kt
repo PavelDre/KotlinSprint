f676a15517a04acde569b17187b918ac4d555613
Pavel <paveld95@mail.ru>
2024-02-15 22:00:00 +0300

    KS-4-5 rename

--- src/main/kotlin/lesson_4/lesson_4_task_5.kt
+++ src/main/kotlin/lesson_4/lesson_4_task_5.kt
@@ -3,7 +3,7 @@ package org.example.lesson_4
 fun main() {
 
     print("Корабль имеет незначительные повреждения? (true/false): ")
-    val damageShip: Boolean = readln().toBoolean()
+    val isDamaged: Boolean = readln().toBoolean()
 
     print("Введите количество членов экипажа корабля (желательно от 55 до 70): ")
     val numberOfCrew: Int = readln().toInt()
@@ -12,17 +12,17 @@ fun main() {
     val numberOfBoxes: Int = readln().toInt()
 
     print("Какие сегодня метеоусловия? Благоприятные или не очень? (true/false): ")
-    val weatherConditions: Boolean = readln().toBoolean()
+    val isFavorable: Boolean = readln().toBoolean()
 
     println("Корабль может отправляться в плавание: " +
-            "${(damageShip == DAMAGE_SHIP && numberOfCrew >= MIN_NUMBER_OF_CREW &&
+            "${(isDamaged == IS_DAMAGED && numberOfCrew >= MIN_NUMBER_OF_CREW &&
                     numberOfCrew <= MAX_NUMBER_OF_CREW && numberOfBoxes > MIN_NUMBER_OF_BOXES) ||
                     (numberOfCrew == MAX_NUMBER_OF_CREW && numberOfBoxes >= MIN_NUMBER_OF_BOXES && 
-                            weatherConditions == WEATHER_CONDITIONS)}")
+                            isFavorable == IS_FAVORABLE)}")
 }
 
-const val DAMAGE_SHIP = false
+const val IS_DAMAGED = false
 const val MIN_NUMBER_OF_CREW = 55
 const val MAX_NUMBER_OF_CREW = 70
 const val MIN_NUMBER_OF_BOXES = 50
-const val WEATHER_CONDITIONS = true
+const val IS_FAVORABLE = true
