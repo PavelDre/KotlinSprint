b9628d7170d37d8b31e93f8cfcbef62964c91ef2
Pavel <paveld95@mail.ru>
2024-02-15 21:36:29 +0300

    KS-4-3 rename variables and constant

--- src/main/kotlin/lesson_4/lesson_4_task_3.kt
+++ src/main/kotlin/lesson_4/lesson_4_task_3.kt
@@ -2,17 +2,17 @@ package org.example.lesson_4
 
 fun main() {
 
-    val sunnyWeather = true
-    val outdoorAwning = true
+    val isSunny = true
+    val isAwningOpen = true
     val percentageAirHumidity = 20
     val season = "зима"
 
     println("Благоприятные ли условия сейчас для роста бобовых? " +
-            "${sunnyWeather == SUNNY_WEATHER && outdoorAwning == OUTDOOR_AWNING && 
+            "${isSunny == IS_SUNNY && isAwningOpen == IS_AWNING_OPEN && 
                     percentageAirHumidity >= MIN_AIR_HUMIDITY && season != SEASON}")
 }
 
-const val SUNNY_WEATHER = true
-const val OUTDOOR_AWNING = true
+const val IS_SUNNY = true
+const val IS_AWNING_OPEN = true
 const val MIN_AIR_HUMIDITY = 20
 const val SEASON = "зима"
