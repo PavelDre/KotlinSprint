0b1d0c3d6bb7b93796811a56cbe7f03e3e119ceb
Pavel <paveld95@mail.ru>
2024-02-16 20:40:32 +0300

    KS-3-5 fixed

--- src/main/kotlin/lesson_3/lesson_3_task_5.kt
+++ src/main/kotlin/lesson_3/lesson_3_task_5.kt
@@ -3,13 +3,10 @@ package org.example.lesson_3
 fun main() {
 
     val lineStroke = "D2-D4;0"
+    val resultLineStroke = lineStroke.split("-", ";")
 
-    val whereFromGo = (lineStroke.split("-", ";"))[0]
-    val whereGo = (lineStroke.split("-", ";"))[1]
-    val numberStroke = (lineStroke.split("-", ";"))[2]
-
-    println(whereFromGo)
-    println(whereGo)
-    println(numberStroke)
+    println(resultLineStroke[0])
+    println(resultLineStroke[1])
+    println(resultLineStroke[2])
 
 }
